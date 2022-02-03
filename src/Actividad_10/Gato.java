563f8a9974512e5ddc385bc9c3c0681c9097959d
15Carmen <carmen.martin@iesnervion.es>
2022-02-03 23:31:16 +0100

    commit 9

---
+++ src/Actividad_10/Gato.java
@@ -0,0 +1,17 @@
+package Actividad_10;
+
+public class Gato {
+    /**
+     * Actividad 10.-
+     * Implementar una clase Gato con las siguientes características:
+     *
+     *  Atributos (String) nombre, color, raza, (Int) edad
+     *
+     *  Constructores necesarios
+     *
+     *  Que implemente dos método CompareTo para compararlos por nombre y por edad
+     *
+     *  Haced un programa principal sencillo que cree dos gatos y pruebe los métodos de
+     * comparación.
+     */
+}
---
+++ src/Actividad_7/Fraccion.java
@@ -0,0 +1,18 @@
+package Actividad_7;
+
+public class Fraccion {
+
+    /**
+     * Actividad 7.-
+     * Crea la clase Fracción. Los atributos, en principio, serán numerador y denominador.
+     *
+     * El numerador y denominador se introducirán por pantalla. No es necesario usar Try pero sí
+     * controlar que el denominador no sea negativo.
+     *
+     * Se crearán métodos para simplificar la fracción, multiplicarla y dividirla por un número
+     * y por una fracción e invertirle el signo.
+     *
+     * Para simplificar la fracción ayudaros del método Math.min que devuelve el mínimo de dos
+     * números (el tipo devuelto es el mismo tipo de los números que compara)
+     */
+}
