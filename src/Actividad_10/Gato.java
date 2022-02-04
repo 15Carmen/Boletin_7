bcfc0dc1f04018aa75682d8d40ac9289a7106dc2
Carmen Martín Núñez <carmen.martin@iesnervion.es>
2022-02-04 12:41:25 +0100

    commit 10

--- src/Actividad_10/Gato.java
+++ src/Actividad_10/Gato.java
@@ -1,7 +1,6 @@
 package Actividad_10;
 
-public class Gato {
-    /**
+    /*
      * Actividad 10.-
      * Implementar una clase Gato con las siguientes características:
      *
@@ -14,4 +13,54 @@ public class Gato {
      *  Haced un programa principal sencillo que cree dos gatos y pruebe los métodos de
      * comparación.
      */
-}
+
+    public class Gato{
+
+        private String nombre;
+        private String color;
+        private String raza;
+        private int edad;
+
+        //constructor
+
+        public Gato(String nombre, String color, String raza, int edad) {
+            this.nombre = nombre;
+            this.color = color;
+            this.raza = raza;
+            this.edad = edad;
+        }
+
+        //getter
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public String getRaza() {
+            return raza;
+        }
+
+        public int getEdad() {
+            return edad;
+        }
+
+        //metodos para comparar los gatos.
+        //hacemos la compración con un compareTo
+
+
+        public int compareToNombres(Gato gatito) {
+            return (this.nombre).compareTo(gatito.getNombre());
+        }
+        public int compareToEdad(Gato gatito) {
+            return this.edad-gatito.edad;
+        }
+
+        public String toString(){
+            return "Nombre: "+ this.nombre+ " Raza: "+ this.raza+ " Color: "+ this.color + " Edad: "+ this.edad;
+        }
+
+    }
--- src/Actividad_10/Main.java
+++ src/Actividad_10/Main.java
@@ -1,4 +1,33 @@
 package Actividad_10;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Gato gatete1=new Gato("Antonia", "rosa", "sphynx", 4);
+        Gato gatete2=new Gato("Chispa", "negro", "scottish fold",9);
+
+        //comparamos a los dos gatitos
+
+        System.out.println("Ordenamos a los gatitos por orden alfabético");
+
+        if (gatete1.compareToNombres(gatete2)<0) {
+            System.out.println(gatete1);
+            System.out.println(gatete2);
+        }else{
+            System.out.println(gatete2);
+            System.out.println(gatete1);
+        }
+
+        System.out.println("\nOrdenamos a los gatitos de mayor a menor edad");
+
+
+        if (gatete1.compareToEdad(gatete2)<0) {
+            System.out.println(gatete2);
+            System.out.println(gatete1);
+        }else{
+            System.out.println(gatete2);
+            System.out.println(gatete1);
+        }
+    }
 }
